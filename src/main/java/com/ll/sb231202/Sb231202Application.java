63eb23be0123921b52af8cb09682a39ea8ef2149
insung5189 <insung5189@gmail.com>
2023-12-02 19:07:42 +0900

    setting

---
+++ src/main/java/com/ll/sb231202/Sb231202Application.java
@@ -0,0 +1,13 @@
+package com.ll.sb231202;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb231202Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb231202Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/sb231202/Sb231202ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sb231202;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb231202ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
