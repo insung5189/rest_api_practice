9809986915140b11a86a128502cf9fa04639cea2
insung5189 <insung5189@gmail.com>
2023-12-05 18:46:34 +0900

    setting2

--- src/test/java/com/ll/sb231202/Sb231202ApplicationTests.java
+++ src/test/java/com/ll/sb231202/Sb231202ApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.sb231202;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Sb231202ApplicationTests {
 
 	@Test
