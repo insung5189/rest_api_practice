3a4f06046fd49289f844955ba2429c53edd9b897
insung5189 <insung5189@gmail.com>
2023-12-07 15:19:09 +0900

    step 5

--- src/main/java/com/ll/sb231202/Sb231202Application.java
+++ src/main/java/com/ll/sb231202/Sb231202Application.java
@@ -2,8 +2,10 @@ package com.ll.sb231202;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Sb231202Application {
 
 	public static void main(String[] args) {
